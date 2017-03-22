1398277951ee6204afac13183143f64af4eac094
RedPer <redperkontakt@gmail.com>
2017-03-22 20:10:34 +0100

    Made some changes to version system. Now versions won't be hardcoded in mcmod.info and Reference.java, but gradle while building will place correct information into this files based on gradle.properties. Also removed some unnecessary files.

--- src/main/java/redper/minecraft/moreoremod/Reference.java
+++ src/main/java/redper/minecraft/moreoremod/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
 	public static final String MOD_ID = "redpermoreoremod";
 	public static final String NAME = "More Ore Mod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String MC_VERSIONS = "[1.11, 1.11.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "redper.minecraft.moreoremod.proxy.ClientProxy";
